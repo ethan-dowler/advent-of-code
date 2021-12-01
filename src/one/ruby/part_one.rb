ed3871267b15be1d4c247ea95b14d2735d02e5a8
Ethan Dowler <ethan.dowler@daveramsey.com>
2021-12-01 10:13:54 -0600

    add day one part one

---
+++ src/one/ruby/part_one.rb
@@ -0,0 +1,18 @@
+# https://adventofcode.com/2021/day/1
+
+require 'csv'
+
+# 🍎 TIL __dir__ is a reference to the directory of the file that's running!
+depths = CSV.parse(File.read("#{__dir__}/../depths.csv")).flatten.map(&:to_i)
+
+number_of_increases = 0
+
+# we don't want to count the first measurement as an increase
+previous_depth = depths.first
+depths[1..].each do |depth|
+  number_of_increases += 1 if depth > previous_depth
+
+  previous_depth = depth
+end
+
+puts number_of_increases
