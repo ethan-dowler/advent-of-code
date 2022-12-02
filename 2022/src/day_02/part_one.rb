42e826213944a5aa8a80c28e99a21d7b01bd20a1
Ethan Dowler <ethan.dowler@daveramsey.com>
2022-12-02 10:07:13 -0600

    update part_one

--- 2022/src/day_02/part_one.rb
+++ 2022/src/day_02/part_one.rb
@@ -5,8 +5,8 @@ File.open("#{__dir__}/strategy_guide.txt", "r") do |file|
   score =
     file.each_line.reduce(0) do |total, line|
       letters = line.split(" ")
-      call, response = letters.map { |letter| Weapon.from_letter(letter) }
-      total + response.fight(call)
+      opponent_weapon, my_weapon = letters.map { |letter| Weapon.from_letter(letter) }
+      total + my_weapon.fight(opponent_weapon)
     end
 
   puts score
